d300d75752262d85e72bd2ed1288c02ae9219a84
tuannam <namnguyen170681@gmail.com>
2025-10-07 20:43:09 +0700

    Review Project

--- src/main/java/com/practice/Application.java
+++ src/main/java/com/practice/Application.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
-		System.out.println("day la branch main");
 		SpringApplication.run(Application.class, args);
 	}
 }
